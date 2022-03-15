41c9065d52a92bb763f41d11cc9aab43891de136
LoufaSupreme <josh.davis@live.ca>
2022-03-14 22:59:45 -0400

    setup resets.css

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import EmailField
+from django.utils import timezone
 
 # Create your models here.
+
+class Guest(models.model):
+    name = models.CharField(max_length=20)
+    alias = models.CharField(max_length=20,blank=True)
+    email = EmailField(max_length=100, blank=True)
+    timestamp = models.DateTimeField(default=timezone.now)
