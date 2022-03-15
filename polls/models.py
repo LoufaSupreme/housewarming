dd1b0ab53a240606ded2a4ff84d7c23b0df7db0a
LoufaSupreme <josh.davis@live.ca>
2022-03-14 23:03:50 -0400

    made index view and migrated

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Guest(models.model):
+class Guest(models.Model):
     name = models.CharField(max_length=20)
     alias = models.CharField(max_length=20,blank=True)
     email = EmailField(max_length=100, blank=True)
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import json, datetime
+
+from .models import Guest
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'polls/index.html')
