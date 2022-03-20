959ab75047090707628fac4bc38f8014a1d54f5e
LoufaSupreme <josh.davis@live.ca>
2022-03-19 22:56:22 -0400

    added question and choice models

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,48 @@ from django.utils import timezone
 # Create your models here.
 
 class Guest(models.Model):
-    name = models.CharField(max_length=20)
-    alias = models.CharField(max_length=20,blank=True)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    alias = models.CharField(max_length=20, blank=True)
+    fav_color = models.CharField(max_length=20, blank=True)
     email = EmailField(max_length=100, blank=True)
     timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.id}: {self.first_name} "{self.alias}" {self.last_name}'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'alias': self.alias,
+            'email': self.email,
+        }
+
+class Question(models.Model):
+    q_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.id}: {self.q_text[:20]}...'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'q_text': self.q_text,
+        }
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=255)
+    votes = models.ManyToManyField(Guest, related_name='choices')
+
+    def __str__(self):
+        return f'{self.id}: {self.choice_text[:10]}... for Q{self.question}'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'choice_text': self.choice_text,
+            'question': self.question.serialize(),
+            'votes': [{'id':guest.id, 'name':guest.name} for guest in self.votes]
+        }
