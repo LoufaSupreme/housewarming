ea3df7f684cfebae23d605fbb9900fdd0a1aae7e
LoufaSupreme <josh.davis@live.ca>
2022-04-03 23:01:47 -0400

    create now makes new question and choices

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-import json, datetime
+import json, datetime, traceback
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 from .models import Guest, Question, Choice
 
@@ -26,6 +27,32 @@ def index(request):
 
 def create(request):
     if request.method == 'GET':
-        return render(request, 'polls/create.html', {
-            
-        })
+        return render(request, 'polls/create.html', {})
+
+    elif request.method == 'POST':
+        try:
+            question = request.POST['question-text']
+            q_type = request.POST['type']
+            try:
+                img = request.FILES['q-img']
+            except:
+                img = None
+            options = [request.POST['option-1'],request.POST['option-2'],request.POST['option-3'],request.POST['option-4'],request.POST['option-5']]
+
+            if question is not None and question is not "":
+                question = Question(q_text=question, type=q_type)
+                if img is not None:
+                    question.image = img
+                question.save()
+
+                for option in options:
+                    if option is not None and option is not "":
+                        option = Choice(question=question, choice_text=option)
+                        option.save()
+
+            print(f'Saved new question: {question}')
+            return render(request, 'polls/create.html', {})
+        except Exception as e:
+            traceback.print_exc()
+
+
