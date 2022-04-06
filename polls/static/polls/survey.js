92608c1df94cc743c547b6827b78315355c0f443
LoufaSupreme <josh.davis@live.ca>
2022-04-05 21:23:55 -0400

    added functionality to nav dots

--- polls/static/polls/survey.js
+++ polls/static/polls/survey.js
@@ -3,8 +3,8 @@ const slides = Array.from(track.querySelectorAll('.poll-container'))
 const startBtn = document.querySelector('.btn-start')
 const nextBtns = Array.from(document.querySelectorAll('.btn-next'))
 const previousBtns = Array.from(document.querySelectorAll('.btn-prev'))
-const carouselNav = document.querySelector('.carousel-nav')
-const indicatorDots = Array.from(carouselNav.children)
+const dotsNav = document.querySelector('.carousel-nav')
+const indicatorDots = Array.from(dotsNav.children)
 
 const slideWidth = slides[0].getBoundingClientRect().width
 
@@ -17,18 +17,36 @@ const moveSlidesForward = () => {
     const currentSlide = track.querySelector('.current-slide')
     const targetSlide = currentSlide.nextElementSibling
     moveToSlide(currentSlide, targetSlide)
+
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const targetDot = currentDot.nextElementSibling
+    updateDots(currentDot, targetDot)
+
 }
 
 const moveSlidesBackwards = () => {
     const currentSlide = track.querySelector('.current-slide')
     const targetSlide = currentSlide.previousElementSibling
     moveToSlide(currentSlide, targetSlide)
+
+    const currentDot = dotsNav.querySelector('.current-slide')
+    const targetDot = currentDot.previousElementSibling
+    updateDots(currentDot, targetDot)
 }
 
 const moveToSlide = (currentSlide, targetSlide) => {
     track.style.transform = `translateX(-${targetSlide.style.left})`
     currentSlide.classList.remove('current-slide')
     targetSlide.classList.add('current-slide')
+}
+
+const clickDots = () => {
+    
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide')
+    targetDot.classList.add('current-slide')
 }
 
 // arrange all slides next to eachother:
@@ -44,5 +62,16 @@ nextBtns.forEach(btn => {
 previousBtns.forEach(btn => btn.addEventListener('click', moveSlidesBackwards))
 
 indicatorDots[0].classList.add('current-slide')
+indicatorDots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+        const currentSlide = track.querySelector('.current-slide')
+        const currentDot = dotsNav.querySelector('.current-slide')
+        const targetDot = e.target
+        const targetIndex = indicatorDots.findIndex(dot => dot === targetDot)
+        const targetSlide = slides[targetIndex]
+        moveToSlide(currentSlide, targetSlide)
+        updateDots(currentDot, targetDot)
+    })
+})
 
 // document.addEventListener('click', e => console.log(e.target))
