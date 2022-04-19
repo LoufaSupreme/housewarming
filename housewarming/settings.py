c7cd995632740ba9a44315b8ea9bd9944e815ffc
LoufaSupreme <josh.davis@live.ca>
2022-04-18 23:57:19 -0400

    installed django-heroku module and updated settings

--- housewarming/settings.py
+++ housewarming/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os, mimetypes
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 
 mimetypes.add_type("text/css", ".css", True)
+
+django_heroku.settings(locals())
