ad901186c3e0234063a5a3488bed2fca6694ac62
LoufaSupreme <josh.davis@live.ca>
2022-04-20 21:42:01 -0400

    added a horizontal filter to admin page for choice votes

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Guest, Question, Choice
 
+# custom formatting for the admin page:
+# note: you need that comma after the argument in filter_horizontal (must be a tuple)
+class ChoiceAdmin(admin.ModelAdmin):
+    filter_horizontal = ('votes',)
+
 admin.site.register(Guest)
 admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
